3c6277efbb9325ddff0d39bf2a28d3ec115fe338
Artem Egorov <doulikehurtingotherpeople@gmail.com>
2020-11-17 15:36:11 +0300

    basic express app with mongodb

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+require("dotenv").config();
+require("./helpers/init_mongodb");
+
+const AuthRoute = require("./routes/Auth");
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/", async (req, res, next) => {
+  res.send("Hello from express");
+});
+
+app.use("/auth", AuthRoute);
+
+// path not found error
+app.use(async (req, res, next) => {
+  next(createError.NotFound());
+});
+
+// обработка ошибок
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/helpers/init_mongodb.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    dbName: process.env.DB_NAME,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => console.log(err.message));
+
+// events
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to db");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err.message);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose connection is disconnected");
+});
+
+// fired when ctrl + c is pressed
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});

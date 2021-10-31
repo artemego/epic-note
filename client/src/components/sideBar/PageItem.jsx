44ad5ec91fdb8d34a6773a25578ba2b6bfd08103
Artem Egorov <doulikehurtingotherpeople@gmail.com>
2021-10-31 20:27:08 +0300

    page item text overflow fix

--- client/src/components/sideBar/PageItem.jsx
+++ client/src/components/sideBar/PageItem.jsx
@@ -29,7 +29,8 @@ function PageItem({
   return (
     <Box
       onClick={(e) => {
-        if (e.target.nodeName === "svg") return;
+        if (e.target.nodeName === "svg" || e.target.nodeName === "BUTTON")
+          return;
         handlePageClick(page.pageId);
       }}
       p="15px 5px"
@@ -37,6 +38,7 @@ function PageItem({
       w="100%"
       h="100%"
       display="flex"
+      // bgColor="black"
     >
       {isDeleting && deletingId === page.pageId && (
         <Spinner color="red.500" position="absolute" right="50%" />
@@ -46,38 +48,45 @@ function PageItem({
         color={pageId === page.pageId ? "orange.600" : ""}
         style={{ transition: ".2s ease-in-out" }}
         textDecor={pageId === page.pageId ? "underline" : ""}
+        flexBasis="80%"
+        maxWidth="80%"
+        whiteSpace="nowrap"
+        overflow="hidden"
+        textOverflow="ellipsis"
       >
         {page.name}
       </Heading>
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Options"
-          icon={<HamburgerIcon />}
-          size="xs"
-          variant="outline"
-          ml="auto"
-        />
-        <MenuList>
-          <MenuItem
-            onClick={() => {
-              // console.log("clicked");
-              handlePageDelete(page.pageId);
-            }}
-            icon={<DeleteIcon />}
-            command="⌘T"
-            // onClick={handlePageDelete(page.id)}
-          >
-            Delete page
-          </MenuItem>
-          <MenuItem icon={<CopyIcon />} command="⌘N">
-            Duplicate
-          </MenuItem>
-          <MenuItem icon={<EditIcon />} command="⌘N">
-            Change name
-          </MenuItem>
-        </MenuList>
-      </Menu>
+      <Box position="absolute" right="5px">
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<HamburgerIcon />}
+            size="xs"
+            variant="outline"
+            ml="auto"
+          />
+          <MenuList>
+            <MenuItem
+              onClick={() => {
+                // console.log("clicked");
+                handlePageDelete(page.pageId);
+              }}
+              icon={<DeleteIcon />}
+              command="⌘T"
+              // onClick={handlePageDelete(page.id)}
+            >
+              Delete page
+            </MenuItem>
+            <MenuItem icon={<CopyIcon />} command="⌘N">
+              Duplicate
+            </MenuItem>
+            <MenuItem icon={<EditIcon />} command="⌘N">
+              Change name
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Box>
     </Box>
   );
 }
